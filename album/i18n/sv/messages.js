15e761926a916747b59c1b1e11a139fb6c6ba9f4
Carl Fürstenberg <azatoth@gmail.com>
2012-01-25 15:46:08 +0100

    Fix encoding on Swedish translation files (Closes: #1869)
    The swedish translation files was encoded ISO-8859-1 instead of
    UTF-8.

--- album/i18n/sv/messages.js
+++ album/i18n/sv/messages.js
@@ -1,14 +1,14 @@
 messages = {
-	'next': 'Nästa',
+	'next': 'NÃ¤sta',
 	'back': 'Tillbaka',
 	'ui-albummanager-album-name' : "Albumnamn",
 	'ui-albummanager-album-caption' : "Beskrivning",
 	'ui-albummangaer-images-in-album' : "i",
 	/* Topnav */
-	"topnav-authorized-bubba" : "Inloggad som " + config.name + " användare '%s'",
-	"topnav-authorized-album" : "Tittar på album inloggad som '%s'",
+	"topnav-authorized-bubba" : "Inloggad som " + config.name + " anvÃ¤ndare '%s'",
+	"topnav-authorized-album" : "Tittar pÃ¥ album inloggad som '%s'",
 	"topnav-authorized" : "Inloggad som '%s'",
-	"topnav-not-authorized" : "Tittar på album anonymt",
+	"topnav-not-authorized" : "Tittar pÃ¥ album anonymt",
 	"topnav-login" : "Logga in",
 	"topnav-logout" : "Logga ut",
 	/* Log out */
@@ -19,7 +19,7 @@ messages = {
 	/* Log in */
 	"login-dialog-continue" : "Logga in",
 
-	"albummanager-move-notice": "Välj destination %d album och %d bilder?",
+	"albummanager-move-notice": "VÃ¤lj destination %d album och %d bilder?",
 	"albummanager-move-no": "Nej",
 	"albummanager-move-yes": "Ja",
 
@@ -27,65 +27,65 @@ messages = {
 	"albummanager-create-dialog-button-label": "Skapa",
 	"albummanager-perm-dialog-button-label": "Uppdatera",
 	"albummanager-modify-dialog-button-label": "Uppdatera",
-	"albummanager-add-dialog-button-label": "Lägg till",
+	"albummanager-add-dialog-button-label": "LÃ¤gg till",
 
 	"album-image-count": "%d bilder",
 	"album-image-subalbum-count": "%d bilder och %d sub-album",
 
 	/* Permissions */
-	"albummanager-label-public": "Tillåta anonym åtkomst?",
-	"albummanager-label-recursive": "Ställa åtkomst vidare upp/ner i trädet?",
+	"albummanager-label-public": "TillÃ¥ta anonym Ã¥tkomst?",
+	"albummanager-label-recursive": "StÃ¤lla Ã¥tkomst vidare upp/ner i trÃ¤det?",
 	"album-users-entry":"%s (%s)",
-	"album-users-delete-message": "Ta bort användare <strong>%s</strong> (\"%s\")?",
+	"album-users-delete-message": "Ta bort anvÃ¤ndare <strong>%s</strong> (\"%s\")?",
 
 	/* User manager */
 	"albummanager-del-no": "Avbryt",
-	"albummanager-del-yes": "Ta bort användare",
-	"albummanager-users-add": "Lägg till användare",
-	"albummanager-button-edit": "Ändra",
+	"albummanager-del-yes": "Ta bort anvÃ¤ndare",
+	"albummanager-users-add": "LÃ¤gg till anvÃ¤ndare",
+	"albummanager-button-edit": "Ã„ndra",
 	
 	
 	/* Help messages */
-	"help-box-header" : config.name + " Album-hjälp",
+	"help-box-header" : config.name + " Album-hjÃ¤lp",
 	"help-info::anon::main":
-	"<h3>Att titta på foton</h3>"
+	"<h3>Att titta pÃ¥ foton</h3>"
 	+"<ul>"
-		+"<li>För att gå in i ett album, dubbelkloicka på det eller använd pilen till höger på sidan.</li>"
-		+"<li>Klicka på en bild för att se den i fullstorlek, och klicka 'bildspel' för att låta bilderna bläddra själva.</li>"
+		+"<li>FÃ¶r att gÃ¥ in i ett album, dubbelkloicka pÃ¥ det eller anvÃ¤nd pilen till hÃ¶ger pÃ¥ sidan.</li>"
+		+"<li>Klicka pÃ¥ en bild fÃ¶r att se den i fullstorlek, och klicka 'bildspel' fÃ¶r att lÃ¥ta bilderna blÃ¤ddra sjÃ¤lva.</li>"
 	+"</ul>"
 	
-	+"<h3>Logga in för att...</h3>"
+	+"<h3>Logga in fÃ¶r att...</h3>"
 	+"<ul>"
 		+"<li>Se privata album.</li>"
 		+"<li>Skapa fotoalbum.</li>"
-		+"<li>Ändra fotoalbum.</li>"
+		+"<li>Ã„ndra fotoalbum.</li>"
 	+"</ul>"
 	
-	+"<p>" + config.name + " användare (inte administratören) kan logga in och skapa album och albumanvändare. Albumanvändare kan endast se på album, inte skapa nya.</p>"
+	+"<p>" + config.name + " anvÃ¤ndare (inte administratÃ¶ren) kan logga in och skapa album och albumanvÃ¤ndare. AlbumanvÃ¤ndare kan endast se pÃ¥ album, inte skapa nya.</p>"
 	,
 	
 	"help-info::user::main":
-	"<h3>Se på foton</h3>"
+	"<h3>Se pÃ¥ foton</h3>"
 	+"<ul>"
-		+"<li>För att gå in i ett album, dubbelkloicka på det eller använd pilen till höger på sidan.</li>"
-		+"<li>Klicka på en bild för att se den i fullstorlek, och klicka 'bildspel' för att låta bilderna bläddra själva.</li>"
+		+"<li>FÃ¶r att gÃ¥ in i ett album, dubbelkloicka pÃ¥ det eller anvÃ¤nd pilen till hÃ¶ger pÃ¥ sidan.</li>"
+		+"<li>Klicka pÃ¥ en bild fÃ¶r att se den i fullstorlek, och klicka 'bildspel' fÃ¶r att lÃ¥ta bilderna blÃ¤ddra sjÃ¤lva.</li>"
 	+"</ul>"
 	
 	+"<h3>Hantera bilder</h3>"
-	+"<p>Klicka på knappen med skiftnyckelsymbolen för att gå in i 'hanteringsläge'. Du måste vara inloggad som en vanlig B3-användare (inte administratör).</p>"
+	+"<p>Klicka pÃ¥ knappen med skiftnyckelsymbolen fÃ¶r att gÃ¥ in i 'hanteringslÃ¤ge'. Du mÃ¥ste vara inloggad som en vanlig B3-anvÃ¤ndare (inte administratÃ¶r).</p>"
 	,
 	
 	"help-info::manager::main":
 	"<h3>Menyalternativ</h3>"
-	+"<p>Observera att du måste markera ett album eller foto för att vissa av funktionerna ska gå att välja.</p>"
+	+"<p>Observera att du mÃ¥ste markera ett album eller foto fÃ¶r att vissa av funktionerna ska gÃ¥ att vÃ¤lja.</p>"
 	+"<ul>"
-		+"<li><strong>Skapa album</strong> - Lägg till ett nytt album. Innan du gör detta måste bilderna vara lagrade i /home/storage/pictures-katalogen.</li>"
-		+"<li><strong>Lägg till bilder</strong> - Lägger till bilder till ett befintligt album.</li>"
-		+"<li><strong>Flytta</strong> - Flyttar ett album eller foto. Markera en eller flera album/foton och klicka Flytta, gå sedan  till önskad destination och bekräfta med Ja-knappen.</li>"
-		+"<li><strong>Döp om</strong> - Döp om eller redigera beskrivningen för ett album eller foto.</li>"
-		+"<li><strong>Åtkomst</strong> - Ändra albumrättigheter. Bestäm vem som ska kunna titta på ditt album. Ställ olika åtkomsträttigheter för olika album. För att låta alla titta, välj 'Tillåt anonym åtkomst'. Du kan ändra åtkomst för flera album samtidigt (sub-album kommer att få samma åtkomsträttigheter som deras moderalbum).</li>"
-		+"<li><strong>Hantera användare</strong> - Skapa och redigera albumanvändare. Albumanvändare kan endast se album, inte logga in på B3 eller använda andra tjänster på B3. Tips! Skapa olika albumanvändare med tillgång till olika album.</li>"
-		+"<li><strong>Ta bort</strong> - Ta bort album eller foton. Fotona kommer inte att försvinna från B3, endast från albumet.</li>"
+		+"<li><strong>Skapa album</strong> - LÃ¤gg till ett nytt album. Innan du gÃ¶r detta mÃ¥ste bilderna vara lagrade i /home/storage/pictures-katalogen.</li>"
+		+"<li><strong>LÃ¤gg till bilder</strong> - LÃ¤gger till bilder till ett befintligt album.</li>"
+		+"<li><strong>Flytta</strong> - Flyttar ett album eller foto. Markera en eller flera album/foton och klicka Flytta, gÃ¥ sedan  till Ã¶nskad destination och bekrÃ¤fta med Ja-knappen.</li>"
+		+"<li><strong>DÃ¶p om</strong> - DÃ¶p om eller redigera beskrivningen fÃ¶r ett album eller foto.</li>"
+		+"<li><strong>Ã…tkomst</strong> - Ã„ndra albumrÃ¤ttigheter. BestÃ¤m vem som ska kunna titta pÃ¥ ditt album. StÃ¤ll olika Ã¥tkomstrÃ¤ttigheter fÃ¶r olika album. FÃ¶r att lÃ¥ta alla titta, vÃ¤lj 'TillÃ¥t anonym Ã¥tkomst'. Du kan Ã¤ndra Ã¥tkomst fÃ¶r flera album samtidigt (sub-album kommer att fÃ¥ samma Ã¥tkomstrÃ¤ttigheter som deras moderalbum).</li>"
+		+"<li><strong>Hantera anvÃ¤ndare</strong> - Skapa och redigera albumanvÃ¤ndare. AlbumanvÃ¤ndare kan endast se album, inte logga in pÃ¥ B3 eller anvÃ¤nda andra tjÃ¤nster pÃ¥ B3. Tips! Skapa olika albumanvÃ¤ndare med tillgÃ¥ng till olika album.</li>"
+		+"<li><strong>Ta bort</strong> - Ta bort album eller foton. Fotona kommer inte att fÃ¶rsvinna frÃ¥n B3, endast frÃ¥n albumet.</li>"
 	+"</ul>"
 	,
 
