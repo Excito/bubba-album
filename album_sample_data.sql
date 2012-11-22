9c1d9a47692069cbaa6c894b3653d420be57afe1
Carl Fürstenberg <carl@excito.com>
2012-11-22 10:01:30 +0100

    update default album mysql

--- album_sample_data.sql
+++ album_sample_data.sql
@@ -1,14 +1,14 @@
-INSERT INTO `album` VALUES (2,'Sample album','','Sample album by Excito Sweden. Photos by Dmitri Popov.',NULL,1,'2011-03-25 14:46:50','0000-00-00 00:00:00');
+INSERT INTO `album` VALUES (2,'Sample album','','Sample album by Excito Sweden. Photos by Dmitri Popov.',NULL,1,'2010-10-04 19:00:12','0000-00-00 00:00:00');
 
 
-INSERT INTO `image` VALUES (11,'/home/storage/pictures/samples/10.jpg',2,'10.jpg',NULL,1986,1324),
-(12,'/home/storage/pictures/samples/5.jpg',2,'5.jpg',NULL,1950,1307),
-(13,'/home/storage/pictures/samples/1.jpg',2,'1.jpg',NULL,1706,1279),
-(14,'/home/storage/pictures/samples/7.jpg',2,'7.jpg','lights, travel, streets, berlin',1685,1123),
-(15,'/home/storage/pictures/samples/2.jpg',2,'2.jpg','macro, stuff',1713,1142),
-(16,'/home/storage/pictures/samples/8.jpg',2,'8.jpg',NULL,1365,2047),
-(17,'/home/storage/pictures/samples/9.jpg',2,'9.jpg',NULL,1950,1307),
-(18,'/home/storage/pictures/samples/4.jpg',2,'4.jpg','macro, plants',1021,1531),
-(19,'/home/storage/pictures/samples/3.jpg',2,'3.jpg',NULL,1811,1207),
-(20,'/home/storage/pictures/samples/6.jpg',2,'6.jpg','flowers',1262,842);
+INSERT INTO `image` VALUES (11,'/home/storage/pictures/samples/10.jpg',2,'10.jpg',NULL,1986,1324,'2010-10-01 18:08:57'),
+(12,'/home/storage/pictures/samples/5.jpg',2,'5.jpg',NULL,1950,1307,'2010-10-01 18:07:05'),
+(13,'/home/storage/pictures/samples/1.jpg',2,'1.jpg',NULL,1706,1279,'2010-10-01 18:08:57'),
+(14,'/home/storage/pictures/samples/7.jpg',2,'7.jpg','lights, travel, streets, berlin',1685,1123,'2010-10-01 18:06:17'),
+(15,'/home/storage/pictures/samples/2.jpg',2,'2.jpg','macro, stuff',1713,1142,'2010-10-01 18:08:57'),
+(16,'/home/storage/pictures/samples/8.jpg',2,'8.jpg',NULL,1365,2047,'2010-10-02 18:10:00'),
+(17,'/home/storage/pictures/samples/9.jpg',2,'9.jpg',NULL,1950,1307,'2010-10-02 13:33:23'),
+(18,'/home/storage/pictures/samples/4.jpg',2,'4.jpg','macro, plants',1021,1531,'2010-10-01 18:07:46'),
+(19,'/home/storage/pictures/samples/3.jpg',2,'3.jpg',NULL,1811,1207,'2010-10-02 16:12:45'),
+(20,'/home/storage/pictures/samples/6.jpg',2,'6.jpg','flowers',1262,842,'2010-10-02 18:08:57');
 
